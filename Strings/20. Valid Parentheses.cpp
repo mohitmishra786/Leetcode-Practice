c7f92274d4226b63e559f45f82590bb62482662e
DuKe786 <71754779+mohitmishra786@users.noreply.github.com>
2021-11-03 08:06:59 +0530

    Create 20. Valid Parentheses.cpp

---
+++ Strings/20. Valid Parentheses.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c : s){
+            if(c == '(' || c == '{' || c == '[') st.push(c);
+            else{
+                if(st.empty()) return false;
+                if(st.top() != '(' and c == ')') return false;
+                if(st.top() != '{' and c == '}') return false;
+                if(st.top() != '[' and c == ']') return false;
+                st.pop();
+            }
+        }
+        
+        return st.empty();
+    }
+};
